1f4547309e49a01248c915cdca590a521e6de13c
Amir Erfanian <amir.shadkam04@gmail.com>
2026-07-22 11:59:33 +0330

    Admin panel configured.

--- Tutorial/myproject/mymodule/urls.py
+++ Tutorial/myproject/mymodule/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from mymodule import views
 
+from django.contrib import admin
+
+admin.site.site_header = "School Administration"
+admin.site.site_title = "School Admin"
+admin.site.index_title = "Welcome to the School Dashboard"
+
 urlpatterns = [
     path('', views.index) ,
     path('about/', views.about),
