61e320cfabef9f231e874158807126493b09eb60
Amir Erfanian <amir.shadkam04@gmail.com>
2026-07-22 18:46:36 +0330

    test

--- blog_portfolio/myblog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from myblog import views
-
-utlpatterns = [
-    path('', views.index, name='home-page'),
-    path('posts', views.posts, name='posts-page'),
-    path('post/<slug:slug>', views.post_detail, name='post-detail-page')
-]
