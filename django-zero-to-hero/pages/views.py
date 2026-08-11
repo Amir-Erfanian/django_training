afc0de77cd5720886b1b784ca76be5280b4720e8
Amir Erfanian <amir.shadkam04@gmail.com>
2026-08-11 18:20:19 +0330

    Models in django

--- django-zero-to-hero/pages/views.py
+++ django-zero-to-hero/pages/views.py
@@ -1,9 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+from django.shortcuts import render, get_object_or_404
+
 
 def home(request):
-    return HttpResponse('hello there')
+    return render(
+        request,
+        "pages/home.html",
+        {
+            "name": "amir",
+        },
+    )
 
 
 def about(request):
-    return HttpResponse('About Up Page')
+    return render(request, "pages/about.html", {"name": "amir"})
+
+
+def product_list(request):
+    products_list = Product.objects.all()
+    return render(request, "pages/product_list.html", {"products": products_list})
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, "pages/product_detail.html", {"product": product})
