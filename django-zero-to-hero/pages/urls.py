22ba2b759c04d174156e97801d5c191ff3cd11aa
Amir Erfanian <amir.shadkam04@gmail.com>
2026-08-11 19:16:03 +0330

    CRUD with styles

--- django-zero-to-hero/pages/urls.py
+++ django-zero-to-hero/pages/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("about/", views.about),
+    path("", views.home, name='home'),
+    path("about/", views.about, name='about'),
     path("products/", views.product_list, name="product-list"),
     path("products/<int:id>/", views.product_detail, name="product-detail"),
     path("products/create/", views.product_create, name="product-create"),
--- django-zero-to-hero/pages/views.py
+++ django-zero-to-hero/pages/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404,
+)
+
 from .models import Product
 from .forms import ProductForm
 
@@ -9,51 +12,85 @@ def home(request):
     return render(
         request,
         "pages/home.html",
-        {
-            "name": "amir",
-        },
     )
 
 
 def about(request):
-    return render(request, "pages/about.html", {"name": "amir"})
+    return render(
+        request,
+        "pages/about.html",
+    )
 
 
 def product_list(request):
-    products_list = Product.objects.all()
-    return render(request, "pages/product_list.html", {"products": products_list})
+    products = Product.objects.all()
+
+    return render(
+        request,
+        "pages/product_list.html",
+        {
+            "products": products,
+        },
+    )
 
 
 def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-    return render(request, "pages/product_detail.html", {"product": product})
+    product = get_object_or_404(
+        Product,
+        id=id,
+    )
+
+    return render(
+        request,
+        "pages/product_detail.html",
+        {
+            "product": product,
+        },
+    )
 
 
 def product_create(request):
+
     if request.method == "POST":
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
-
             return redirect("product-list")
 
     else:
         form = ProductForm()
 
-    return render(request, "pages/product_form.html", {"form": form})
+    return render(
+        request,
+        "pages/product_form.html",
+        {
+            "form": form,
+        },
+    )
 
 
 def product_update(request, id):
-    product = get_object_or_404(Product, id=id)
+    product = get_object_or_404(
+        Product,
+        id=id,
+    )
     if request.method == "POST":
-        form = ProductForm(request.POST, instance=product)
+        form = ProductForm(
+            request.POST,
+            instance=product,
+        )
 
         if form.is_valid():
             form.save()
+            return redirect(
+                "product-detail",
+                id=product.id,
+            )
 
-            return redirect("product-detail", id=product.id)
     else:
-        form = ProductForm(instance=product)
+        form = ProductForm(
+            instance=product,
+        )
 
     return render(
         request,
@@ -66,9 +103,20 @@ def product_update(request, id):
 
 
 def product_delete(request, id):
-    product = get_object_or_404(Product, id=id)
+
+    product = get_object_or_404(
+        Product,
+        id=id,
+    )
+
     if request.method == "POST":
         product.delete()
         return redirect("product-list")
 
-    return render(request, "pages/product_confirm_delete.html", {"product": product})
+    return render(
+        request,
+        "pages/product_confirm_delete.html",
+        {
+            "product": product,
+        },
+    )
