fa0c0f857fc4e20a37f7b8d1d43cbcef7abf400b
Amir Erfanian <amir.shadkam04@gmail.com>
2026-08-11 18:35:41 +0330

    CRUD in django

--- django-zero-to-hero/pages/urls.py
+++ django-zero-to-hero/pages/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("about/", views.about),
     path("products/", views.product_list, name="product-list"),
     path("products/<int:id>/", views.product_detail, name="product-detail"),
+    path("products/create/", views.product_create, name="product-create"),
+    path("products/<int:id>/update/", views.product_update, name="product-update"),
+    path("products/<int:id>/delete/", views.product_delete, name="product-delete"),
 ]
