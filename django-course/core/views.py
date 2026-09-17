11ee85dfadb6f52a2faa6e8699afaf353f16876e
Amir <amirkowh@gmail.com>
2026-09-17 22:04:50 +0330

    mdels and migratinos

--- django-course/core/views.py
+++ django-course/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home(request):
@@ -17,5 +18,25 @@ def about(request):
     return render(request, 'core/about.html')
 
 
+
+
 def contact(request):
-    return render(request, 'core/contact.html')
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+
+            print(name)
+            print(email)
+            print(message)
+
+    else:
+        form = ContactForm()
+
+    return render(request, "core/contact.html", {
+        "form": form,
+    })
